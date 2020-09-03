7d4ab8743a8d7c233acf679ad6784371f5b94988
Jordan Majd <jordan@madtitanstudios.com>
2020-09-02 18:34:59 -0600

    scoreboard

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use amethyst::{
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+};
+
+mod pong;
+mod systems;
+use crate::pong::Pong;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+    let display_config_path = app_root.join("config").join("display.ron");
+    let binding_path = app_root.join("config").join("bindings.ron");
+
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+
+    let rendering_bundle = RenderingBundle::<DefaultBackend>::new()
+        .with_plugin(
+            RenderToWindow::from_config_path(display_config_path)?.with_clear([0.0, 0.0, 0.0, 1.0]),
+        )
+        .with_plugin(RenderFlat2D::default())
+        .with_plugin(RenderUi::default());
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(rendering_bundle)?
+        .with_bundle(input_bundle)?
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallSystem, "ball_system", &[])
+        .with(systems::BounceSystem, "collision_system", &["paddle_system", "ball_system",])
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"]);
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
+    game.run();
+
+    Ok(())
+}
---
+++ src/systems/bounce.rs
@@ -0,0 +1,53 @@
+use amethyst::{
+    core::{SystemDesc, Transform},
+    derive::SystemDesc,
+    ecs::{Join, ReadStorage, System, SystemData, World, WriteStorage},
+};
+
+use crate::pong::{Ball, Paddle, Side, ARENA_HEIGHT};
+
+pub struct BounceSystem;
+
+impl<'s> System<'s> for BounceSystem {
+    type SystemData = (
+        WriteStorage<'s, Ball>,
+        ReadStorage<'s, Paddle>,
+        ReadStorage<'s, Transform>,
+    );
+
+    fn run(&mut self, (mut balls, paddles, transforms): Self::SystemData) {
+        for (ball, transform) in (&mut balls, &transforms).join() {
+            let bx = transform.translation().x;
+            let by = transform.translation().y;
+
+            if (by <= ball.radius && ball.velocity[1] < 0.0)
+                || (by >= ARENA_HEIGHT - ball.radius && ball.velocity[1] > 0.0)
+            {
+                ball.velocity[1] = -ball.velocity[1];
+            }
+
+            for (paddle, transform) in (&paddles, &transforms).join() {
+                let px = transform.translation().x - (paddle.width * 0.5);
+                let py = transform.translation().y - (paddle.height * 0.5);
+                if point_in_rect(
+                    bx,
+                    by,
+                    px - ball.radius,
+                    py - ball.radius,
+                    px + paddle.width + ball.radius,
+                    py + paddle.height + ball.radius,
+                ) {
+                    if (paddle.side == Side::Left && ball.velocity[0] < 0.0)
+                        || (paddle.side == Side::Right && ball.velocity[0] > 0.0)
+                    {
+                        ball.velocity[0] = -ball.velocity[0];
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn point_in_rect(x: f32, y: f32, left: f32, bottom: f32, right: f32, top: f32) -> bool {
+    x >= left && x <= right && y >= bottom && y <= top
+}
